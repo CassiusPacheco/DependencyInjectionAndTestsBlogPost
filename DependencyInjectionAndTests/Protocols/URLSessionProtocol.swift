bbf8b40c72c03ed601bfb66607228cc17480527e
Cassius Pacheco <cassius.pacheco@theiconic.com.au>
2017-09-17 17:32:33 +1000

    Create URLSessionProtocol

---
+++ DependencyInjectionAndTests/Protocols/URLSessionProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  URLSessionProtocol.swift
+//  DependencyInjectionAndTests
+//
+//  Created by Cassius Pacheco on 17/9/17.
+//  Copyright © 2017 Cassius Pacheco. All rights reserved.
+//
+
+import Foundation
+
+protocol URLSessionProtocol: class {
+    
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
+extension URLSession: URLSessionProtocol { }

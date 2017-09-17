0cc6bdb607721529b0de5c9d31e0dc2114c1f600
Cassius Pacheco <cassius.pacheco@theiconic.com.au>
2017-09-17 20:21:52 +1000

    Add tests for PUT data being sent

--- DependencyInjectionAndTestsTests/Clients/HttpClientTests.swift
+++ DependencyInjectionAndTestsTests/Clients/HttpClientTests.swift
@@ -161,4 +161,19 @@ class HttpClientTests: XCTestCase {
         XCTAssertEqual(dictionary["key"] as? String, "value")
     }
     
+    func testPUTBodyDataBeingSent() {
+        
+        let urlSession = URLSessionMock()
+        
+        let client = HttpClient(baseUrl: "baseurl.com", urlSession: urlSession)
+        
+        client.request(endpoint: "/endpoint", httpMethod: .PUT, bodyData: ["key": "value"]) { result in }
+        
+        XCTAssertNotNil(urlSession.requestReceived?.httpBody)
+        
+        let bodyData = urlSession.requestReceived!.httpBody!
+        let dictionary = try! JSONSerialization.jsonObject(with: bodyData, options: .allowFragments) as! JSONDictionary
+        
+        XCTAssertEqual(dictionary["key"] as? String, "value")
+    }
 }

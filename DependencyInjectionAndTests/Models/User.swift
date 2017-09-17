f9accc1ec364ae68db2b3371fb553e107dbb90c1
Cassius Pacheco <cassius.pacheco@theiconic.com.au>
2017-09-17 17:35:43 +1000

    Create User

---
+++ DependencyInjectionAndTests/Models/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  DependencyInjectionAndTests
+//
+//  Created by Cassius Pacheco on 17/9/17.
+//  Copyright © 2017 Cassius Pacheco. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    
+    let name: String
+    
+    init(name: String) {
+        
+        self.name = name
+    }
+}
